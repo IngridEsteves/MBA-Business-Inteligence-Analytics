78761e0acc5260d3f492323c0f41a39571a5e2ef
Ingrid Esteves <ingridestevespereiraiep@gmail.com>
2024-11-13 22:09:10 -0300

    aula 2 com GitHub Copilot

---
+++ Estatistica-Aplicada/aula2.py
@@ -0,0 +1,16 @@
+var1 = 10
+var2 = 50
+
+
+# Criar uma função que recebe dois argumentos e retorna a soma deles
+def soma(a, b):
+    return a + b
+
+
+# Criar uma função que recebe dois argumentos e retorna a multiplicação deles
+def multiplicacao(a, b):
+    return a * b
+
+
+print(soma(var1, var2))
+print(multiplicacao(var1, var2))
